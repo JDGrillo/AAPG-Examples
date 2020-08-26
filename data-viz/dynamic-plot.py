326540e5765d7f084021cbabaa4f38868bfbf1c7
JD Grillo <jd@hellonesh.io>
2020-08-26 01:00:38 -0500

    jupyter notebooks

--- data-viz/dynamic-plot.py
+++ data-viz/dynamic-plot.py
@@ -2,11 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+# can change which file is being used with user input
 # data_file = input("Please enter the name of the data file: ")
 # if data_file.endswith(".csv") != True:
 # 	data_file += ".csv"
 data_file = "AAPG-Dataset-01.csv"
 
+# set up dictionary to know where element references are
+# for example, if the user is wanting to plot Si, we know to look at the 5th column in our csv
 data_dictionary = {
     "depth": 0,
     "na": 1,
@@ -48,13 +51,22 @@ data_dictionary = {
     "kaolinite": 37,
 }
 
+# load in the data to a numpy array
 data = np.genfromtxt(data_file, delimiter=',', skip_header=True)[:, 0:]
 
+# get user input as to what they want to see plotted
+# notice the .lower() function call to match what is in our dictionary
 x = input("Please the first attribute to be plotted: ").lower()
 y = input("Please the second attribute to be plotted: ").lower()
 color = input("Please the attribute to be colored by: ").lower()
 
+# set the size of the figure to be created
+plt.figure(figsize=(18, 12))
+# create a scatter plot using the response from the user, and which dicionary value those response relate to
+# for example, if the user asks for Calcite, data[:,data_dictionary[x]] becomes data[:,32]
+# the [:,] syntax is used to select a 'slice' of data, which in this context is a column from csv
 plt.scatter(data[:,data_dictionary[x]], data[:,data_dictionary[y]], c=data[:,data_dictionary[color]])
 plt.title(f"""{x} vs {y} colored by {color}""")
-
-plt.savefig("Dyanmic-Plot.png")
+plt.show()
+# optionally save figure to a .png
+# plt.savefig("Dyanmic-Plot.png")
--- data-viz/static-plot.py
+++ data-viz/static-plot.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+# can change which file is being used with user input
 # data_file = input("Please enter the name of the data file: ")
 # if data_file.endswith(".csv") != True:
 # 	data_file += ".csv"
 data_file = "AAPG-Dataset-01.csv"
 
+# load in the data to a numpy array
+# similiar to pandas dataframe, just another option
 data = np.genfromtxt(data_file, delimiter=',', skip_header=True)[:, 0:]
 
-with open(data_file, mode="r", encoding="utf-8-sig") as f:
-    d_reader = csv.DictReader(f)
-    data_headers = d_reader.fieldnames
-
-fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+# set up how many subplots we want, and the size of the figure
+fig, axs = plt.subplots(2, 2, figsize=(18, 12))
+# assign each subplot with the data to be shown
 axs[0, 0].scatter(data[:,0], data[:,4], c=data[:,29])
 axs[0, 0].title.set_text("%Si vs. Depth Colored by Quartz")
 axs[1, 0].scatter(data[:,4], data[:,29], c=data[:,29])
@@ -23,4 +24,6 @@ axs[0, 1].title.set_text("%Ca vs. Depth Colored by Calcite")
 axs[1, 1].scatter(data[:,8], data[:,32], c=data[:,32])
 axs[1, 1].title.set_text("%Ca vs. %Calcite Colored by Calcite")
 
-plt.savefig("Static-Plot.png")
+plt.show()
+# optionally save figure to a .png
+#plt.savefig("Static-Plot.png")
